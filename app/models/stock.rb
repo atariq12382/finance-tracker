594e91b03a8732b59e342b8b97324929e4a85829
Abdullah Tariq <mat57557@gmail.com>
2022-08-04 17:39:07 +0500

    275 complete

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -7,11 +11,15 @@ class Stock < ApplicationRecord
     )
     # quote = client.quote(ticker_symbol)
     # quote.latest_price
-    # client.price(ticker_symbol)
+    # client.price(ticker_symbol) 
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
